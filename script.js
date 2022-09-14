52ea4bb9cf675a675915960450044f7f53bf609c
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2022-09-15 01:02:05 +0600

    math.round added

--- script.js
+++ script.js
@@ -134,5 +134,5 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-  document.getElementById("show-time").innerHTML = `${startTime ? parseInt( timeSpent) : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? Math.round( timeSpent) : 0} seconds`;
 }, 1000);
